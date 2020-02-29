f9ec37e4f1998cb2bbd11d306c4179b176a2d8c1
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-02-28 22:12:56 -0300

    Adição de resolução.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicio5_contagemregressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+            Console.WriteLine("FOGO!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" ______ _          _ ");
+            Console.WriteLine("|  ____(_)        | |");
+            Console.WriteLine("| |__   _ _ __ ___| |");
+            Console.WriteLine("|  __| | | '__/ _ | |");
+            Console.WriteLine("| |    | | | |  __/_|");
+            Console.WriteLine("|_|    |_|_|  |___(_)");
+            Console.WriteLine("                     ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+}
